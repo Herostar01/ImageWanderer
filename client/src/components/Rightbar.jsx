3a77f843b89bea034b9c8864ff8095bc1f109bfa
Muhammad Shahbaz <M023Shah@gmail.com>
2022-07-20 14:53:09 -0700

    adding image list

--- client/src/components/Rightbar.jsx
+++ client/src/components/Rightbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar, AvatarGroup, Box, ImageList, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material'
 
 const Rightbar = () => {
 
@@ -40,6 +40,31 @@ return (
 
     </Typography>
     <ImageList>
+
+        <ImageListItem>
+            <img  
+                src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2020/08/12/1513/Hana-Maui-Resort-P001-Garden-Suite-View.jpg/Hana-Maui-Resort-P001-Garden-Suite-View.16x9.jpg"
+                alt=""
+            />
+        </ImageListItem>
+        <ImageListItem >
+            <img  
+                src="https://ak.picdn.net/offset/photos/5d31e837469b183482a1c8e0/medium/photo.jpg"
+                alt=""
+            />
+        </ImageListItem>
+        <ImageListItem>
+            <img 
+                src="https://imgproxy.backpackerdeals.com/1DJAlD_PsCdMfhAsxDEhYhipCzbJ2RT7itih61yU7ls/q:85/w:3000/h:2400/plain/https://assets.travelloapp.com/uploads/deal/road-to-hana-adventure-tour-with-lunch-deal-1.jpg"
+                alt=""
+            />
+        </ImageListItem>
+        <ImageListItem>
+            <img 
+                src="https://fh-sites.imgix.net/sites/959/2018/01/25161348/02-Hana-Home-Page.jpg?auto=compress%2Cformat&w=700&h=700&fit=max"
+                alt=""
+            />
+        </ImageListItem>
 
     </ImageList>
 
